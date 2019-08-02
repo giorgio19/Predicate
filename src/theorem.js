45805254b64cdb27429a67e0215301f9719b6c9b
giorgio19 <giorgio.catania@pepperdin.edu>
2019-08-02 07:06:47 -0700

    changed font and then added in template for proof

--- src/theorem.js
+++ src/theorem.js
@@ -37,24 +37,58 @@ MathJax.Hub.Config({
 	}
 });
 
-MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+MathJax.Hub.Queue(["Typeset", MathJax.Hub, createTable.bible]);
+
 
 
 $(document).ready(function () {
-  createTable();
+	createTable();
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+		acc[i].addEventListener("click", function () {
+			this.classList.toggle("active");
+			var panel = this.nextElementSibling;
+			if (panel.style.maxHeight) {
+				panel.style.maxHeight = null;
+			} else {
+				panel.style.maxHeight = panel.scrollHeight + "px";
+			}
+		});
+	}
 });
 
-function createTable(){
-    var theorems = theoremsStr.theorems;
-    var tr;
-    for (var i = 0; i < theorems.length; i++) {
-        tr = $('<tr/>');
-        tr.append("<td>" + "(" + theorems[i].rule.bold() + ")" + "</td>");
-        theorems[i].name != undefined ? tr.append("<td class = name>" + theorems[i].name + "</td>") : tr.append("<td class = name> </td>");
-        theorems[i].eq != undefined ?
-          tr.append("<td class='eq'>" + theorems[i].eq + "</td>")
-          : tr.append("<td class='eq'>" + theorems[i].eqa + "</td>");
-        $('table').append(tr);
-    }
-
+function createTable() {
+	// $("#accBegin").prepend("<input id='search' type='text' placeholder='Search..'>");
+	// $("#accBegin").append("$\\text{Theorem Sections}$");
+	var theorems = theoremsStr.theorems;
+	var i = 0;
+	var str = "<button class='accordion' style='top:70px;'>" + theorems[i].section + "</button><div class='panel'><table>" +
+		"<tr>" +
+		"<th>Rule</th>" +
+		"<th>Name</th>" +
+		"<th>Equation</th>" +
+		"</tr><tr/>";
+	str += "<tr><td>(" + theorems[i].rule.bold() + ")</td>";
+	theorems[i].name != undefined ? str += "<td class = name>" + theorems[i].name + "</td>" : str += "<td class = name> </td>";
+	str += "<td class='eq'>" + theorems[i].eq + "</td></tr>";
+	for (i = 1; i < theorems.length; i++) {
+		if (theorems[i].section != undefined) {
+			str += "</table></div><button class='accordion'>" + theorems[i].section + "</button><div class='panel'><table>" +
+				"<tr>" +
+				"<th>Rule</th>" +
+				"<th>Name</th>" +
+				"<th>Equation</th>" +
+				"</tr><tr/>";
+			str += "<tr><td>(" + theorems[i].rule.bold() + ")</td>";
+			theorems[i].name != undefined ? str += "<td class = name>" + theorems[i].name + "</td>" : str += "<td class = name> </td>";
+			str += "<td class='eq'>" + theorems[i].eq + "</td></tr>";
+		} else {
+			str += "<tr><td>" + "(" + theorems[i].rule.bold() + ")" + "</td>";
+			theorems[i].name != undefined ? str += "<td class = name>" + theorems[i].name + "</td>" : str += "<td class = name> </td>";
+			str += "<td class='eq'>" + theorems[i].eq + "</td></tr>";
+		}
+	}
+	$("#theorems").append(str);
 }
