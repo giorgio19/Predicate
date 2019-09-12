01fd71dba5d68ef5d79ceb495412904592467f5c
giorgio19 <giorgio.catania@pepperdine.edu>
2019-09-11 19:07:59 -0700

    possible restructure

--- main.js
+++ main.js
@@ -1,115 +1,85 @@
-const { app, BrowserWindow } = require('electron')
 const path = require('path')
-const {ipcMain} = require('electron')
+const glob = require('glob')
+const {app, BrowserWindow} = require('electron')
+const debug = /--debug/.test(process.argv[2])
 
+if (process.mas) app.setName('Predicate')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win = null
-let theoremWin = null
-let open = false;
+let mainWindow = null
 
-const windowsOpt =
-{
-    width: 1215,
-    height: 860,
-    maximizable: false,
-    title: 'Predicate',
-    webPreferences: {
+function initialize() {
+  makeSingleInstance()
+
+  function createWindow () {
+    const windowOptions = {
+      width: 1080,
+      minWidth: 680,
+      height: 840,
+      title: app.getName(),
+      webPreferences: {
         nodeIntegration: true
-    }
-}
-
-const macOpt = {
-    width: 1200,
-    height: 800,
-    maximizable: false,
-    frame: false,
-    webPreferences: {
-        nodeIntegration: true
-    }
-}
-
-const theoremWinOpt = {
-    width: 600,
-    height: 800,
-    maximizable: false,
-    show: false,
-    webPreferences: {
-        nodeIntegration: true
-    }
-};
-const theoremMacOpt = {
-    width: 600,
-    height: 800,
-    maximizable: false,
-    show: false,
-    frame: false,
-    webPreferences: {
-        nodeIntegration: true
-    }
-};
-
-function createWindow () {
-  // Create the browser window.
-    if (process.platform === "win32") {
-      win = new BrowserWindow(windowsOpt);
-      theoremWin = new BrowserWindow(theoremWinOpt);
-    } else {
-      win = new BrowserWindow(macOpt);
-      theoremWin = new BrowserWindow(theoremMacOpt);
+      }
     }
 
-  win.loadFile('./src/index.html')
+    if (process.platform === 'linux') {
+      windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/icon-512.png')
+    }
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
+    mainWindow = new BrowserWindow(windowOptions)
+    mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
+
+    // Launch fullscreen with DevTools open, usage: npm run debug
+    if (debug) {
+      mainWindow.webContents.openDevTools()
+    }
+
+    mainWindow.on('closed', () => {
+      mainWindow = null
+      app.quit()
     })
+  }
 
-  theoremWin.loadFile('./src/theorem.html')
-  theoremWin.on('closed', () =>
-  {
-      theoremWin = null;
+  app.on('ready', () => {
+    createWindow()
+  })
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+
+  app.on('activate', () => {
+    if (mainWindow === null) {
+      createWindow()
+    }
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+// Make this app a single instance app.
+//
+// The main window will be restored and focused instead of a second window
+// opened when a person attempts to launch a second instance.
+//
+// Returns true if the current version of the app should quit instead of
+// launching.
+function makeSingleInstance () {
+  if (process.mas) return
 
-// Quit when all windows are closed.
-app.on('closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+  app.requestSingleInstanceLock()
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+}
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-ipcMain.on('resize', function () {
-    theoremWin.show();
-    process.platform === "win32" ? win.setSize(775, 675) : win.setSize(775,614);
-    open = !open;
-})
+// Require each JS file in the main-process dir
+function loadScripts () {
+  const files = glob.sync(path.join(__dirname, 'main-process/**/*.js'))
+  files.forEach((file) => { require(file) })
+}
 
-ipcMain.on('popIn', function(event, arg) {
-  theoremWin.hide();
-  process.platform === "win32" ? win.setSize(1215,860) : win.setSize(1200,800);
-  win.webContents.send('popIn');
-  open = !open;
-})
+initialize()
